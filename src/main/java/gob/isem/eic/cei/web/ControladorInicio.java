6e2f1849d8c52117ae846a0ff5617bcc3302eeee
jramon208 <jramon208@Debian>
2021-11-15 08:51:02 -0600

    Spring security

--- src/main/java/gob/isem/eic/cei/web/ControladorInicio.java
+++ src/main/java/gob/isem/eic/cei/web/ControladorInicio.java
@@ -6,6 +6,8 @@ import javax.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -23,7 +25,7 @@ public class ControladorInicio {
     private PersonaService personaService;
 
     @GetMapping("/")
-    public String inicio(Model model) {
+    public String inicio(Model model, @AuthenticationPrincipal User user) {
         String mensaje = "Mensaje con Spring y thymeleaf";
 
         var personas = personaService.listarPersonas();
@@ -35,6 +37,7 @@ public class ControladorInicio {
         model.addAttribute("personas", personas);
         log.info(personas.toString());
         log.info("Ejecutando el controlador Spring MVC desde PostgreSQL");
+        log.info("Usuario registrado: " + user);
         return "index";
     }
 
